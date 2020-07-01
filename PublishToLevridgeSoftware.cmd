a0907a363759c3fe5c3555b725e34b9bc7d1f1d5
Tory Bjorklund <tory@levridge.com>
2020-07-01 16:38:13 -0600

    Deployed bf9d5d3 with MkDocs version: 1.0.4

--- PublishToLevridgeSoftware.cmd
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-cd ../../LevridgeSoftware/LevridgeSoftware.github.io
-mkdocs gh-deploy --config-file ../../Levridge-Tory/LevridgeIntegrationDocs/integration.yml --remote-branch master
--- docs/Feed.md
+++
@@ -1,6 +0,0 @@
-﻿## Feed
-
-# Overview
-
-# Main Point 1
-## Sub Point 1.1
